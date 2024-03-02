cd02c6a15dfef315a2ad3dd2dc901f257a5efd95
javad kazemzadeh <ijavad805@gmail.com>
2024-03-02 13:08:34 +0330

    config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-    extends: ['react-app', 'prettier/@typescript-eslint', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
+    extends: [
+        "react-app",
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended",
+        "plugin:storybook/recommended",
+    ],
+    rules: {
+        "react/no-unescaped-entities": [],
+    },
 };
